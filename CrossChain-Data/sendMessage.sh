2735a4357a4d6b328f3d808bc37550299459b9d0
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2025-07-15 08:29:25 +0530

    Starting it all over again

--- CrossChain-Data/sendMessage.sh
+++
@@ -1,8 +0,0 @@
-source .env
-
-cast send $SENDER_ADDRESS \
-  "sendMessage(uint64,address,string)(bytes32)" \
-  $DESTINATION_CHAIN_SELECTOR \
-  $RECEIVER_ADDRESS "Hello from Sender!" \
-  --private-key $PRIVATE_KEY \
-  --rpc-url $ANVIL_RPC_URL
